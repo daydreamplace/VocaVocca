045996ec348d22c18559b33c4238d5b061de0d5f
Eden <you8inpark@gmail.com>
2025-01-08 23:41:15 +0900

    feat: 생성자 호출

--- VocaVocca/View/CustomView/CustomButton.swift
+++ VocaVocca/View/CustomView/CustomButton.swift
@@ -9,5 +9,16 @@ import UIKit
 
 class CustomButton: UIButton {
     
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(title: String) {
+        super.init(frame: .zero)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
