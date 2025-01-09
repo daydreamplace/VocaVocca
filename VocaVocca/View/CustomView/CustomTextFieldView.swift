e8be8a4af8098f2c97cff2d5c74abc3e2d7f31da
Eden <you8inpark@gmail.com>
2025-01-10 04:44:45 +0900

    feat: UIComponents와 initialization 생성

--- VocaVocca/View/CustomView/CustomTextFieldView.swift
+++ VocaVocca/View/CustomView/CustomTextFieldView.swift
@@ -8,6 +8,34 @@
 import UIKit
 import SnapKit
 
-class CustomTextFieldView: UITextField {
+class CustomTextFieldView: UIView, UITextFieldDelegate {
     
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel()
+    private let textField = UITextField()
+    private let borderView = UIView()
+    
+    var text: String? {
+        get { return textField.text }
+        set { textField.text = newValue }
+    }
+    
+    // MARK: - Initialization
+    
+    init(title: String, placeholder: String) {
+        super.init(frame: .zero)
+        setupUI(title: title, placeholder: placeholder)
+        textField.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setup
+    
+    private func setupUI(title: String, placeholder: String) {
+     
+        }
 }
