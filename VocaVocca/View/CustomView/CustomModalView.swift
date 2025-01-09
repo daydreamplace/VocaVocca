1941a70493e1c290e9e9ab56d75edda806e63bc5
Eden <you8inpark@gmail.com>
2025-01-09 15:47:19 +0900

    feat: UIComponents 선언

--- VocaVocca/View/CustomView/CustomModalView.swift
+++ VocaVocca/View/CustomView/CustomModalView.swift
@@ -10,5 +10,25 @@ import SnapKit
 
 class CustomModalView: UIView {
     
+    // MARK: - UI Components
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    let contenStackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
+    
+    private let confirmButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
 }
