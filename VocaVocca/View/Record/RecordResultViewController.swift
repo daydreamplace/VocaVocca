38a983ae3bfc5df0d8a84eae2d92577e7e5bed26
youngjin <bryjna07@nate.com>
2025-01-09 17:18:56 +0900

    style: 스냅킷 $0로 수정

--- VocaVocca/View/Record/RecordResultViewController.swift
+++ VocaVocca/View/Record/RecordResultViewController.swift
@@ -73,36 +73,36 @@ final class RecordResultViewController: UIViewController {
         incorrectView.addSubviews(incorrectLabel, incorrectCountLabel)
         view.addSubviews(correctView, incorrectView)
         
-        correctView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(30)
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
-            make.height.equalTo(160)
+        correctView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(30)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(80)
+            $0.height.equalTo(160)
         }
         
-        correctLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(36)
-            make.centerX.equalToSuperview()
+        correctLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(36)
+            $0.centerX.equalToSuperview()
         }
         
-        correctCountLabel.snp.makeConstraints { make in
-            make.top.equalTo(correctLabel.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
+        correctCountLabel.snp.makeConstraints {
+            $0.top.equalTo(correctLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
         }
         
-        incorrectView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(30)
-            make.top.equalTo(correctView.snp.bottom).offset(60)
-            make.height.equalTo(160)
+        incorrectView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(30)
+            $0.top.equalTo(correctView.snp.bottom).offset(60)
+            $0.height.equalTo(160)
         }
         
-        incorrectLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(36)
-            make.centerX.equalToSuperview()
+        incorrectLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(36)
+            $0.centerX.equalToSuperview()
         }
         
-        incorrectCountLabel.snp.makeConstraints { make in
-            make.top.equalTo(incorrectLabel.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
+        incorrectCountLabel.snp.makeConstraints {
+            $0.top.equalTo(incorrectLabel.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
         }
     }
 }
