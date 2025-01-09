db3f9175aca5b54e1210c03068ce2d5351b9d255
AhnJunGyung <ekdlrkzm@naver.com>
2025-01-09 11:58:46 +0900

    feat: CustomCardView 구현

--- VocaVocca/View/CustomView/CustomCardView.swift
+++ VocaVocca/View/CustomView/CustomCardView.swift
@@ -5,4 +5,80 @@
 //  Created by Eden on 1/8/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class CustomCardView: UIView {
+    
+    let cardBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "black"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textColor = .black
+        return label
+    }()
+    
+    let meanLabel: UILabel = {
+        let label = UILabel()
+        label.text = "검정색"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textColor = .black
+        return label
+    }()
+    
+    let removeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "trash"), for: .normal)
+        button.tintColor = .gray
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+
+        [wordLabel, meanLabel, removeButton].forEach { cardBackgroundView.addSubview($0) }
+        
+        addSubviews(cardBackgroundView)
+        
+        cardBackgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(340)
+            $0.height.equalTo(140)
+        }
+        
+        wordLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.leading.equalTo(cardBackgroundView.snp.leading).inset(20)
+        }
+        
+        meanLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.trailing.equalTo(cardBackgroundView.snp.trailing).inset(20)
+        }
+        
+        removeButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(20)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+}
+
