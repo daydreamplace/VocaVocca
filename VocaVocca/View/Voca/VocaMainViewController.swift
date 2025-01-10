a0f64c54e5a1b8fbbb2accf04d3127bf44396b6b
Eden <you8inpark@gmail.com>
2025-01-10 10:38:42 +0900

    chore: 불필요한 코드 삭제

--- VocaVocca/View/Voca/VocaMainViewController.swift
+++ VocaVocca/View/Voca/VocaMainViewController.swift
@@ -13,16 +13,6 @@ class VocaMainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
-        
-        // CustomTextFieldView 인스턴스 생성
-        let customTextField = CustomTextFieldView(title: "단어장 이름", placeholder: "입력해주세요")
-        
-        // 텍스트필드 추가 및 레이아웃 설정
-        view.addSubview(customTextField)
-        customTextField.snp.makeConstraints { make in
-            make.center.equalToSuperview() // 화면 중앙에 배치
-            make.leading.trailing.equalToSuperview().inset(16) // 좌우 여백
-        }
     }
     
     
