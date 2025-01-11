c1e4a2e6f3c6c02a4285c41c3c5bcd01c607a569
Eden <you8inpark@gmail.com>
2025-01-11 19:59:53 +0900

    feat: selectVocaLabel 구현 및 레이아웃 정의

--- VocaVocca/View/Voca/VocaModalViewController.swift
+++ VocaVocca/View/Voca/VocaModalViewController.swift
@@ -9,34 +9,44 @@ import UIKit
 import SnapKit
 
 class VocaModalViewController: UIViewController {
-    
+
     // MARK: - Properties
-    
+
     private let modalView = CustomModalView(title: "새로운 단어 만들기", buttonTitle: "추가하기")
+    
+    private let selectVocaLabel: UILabel = {
+        let label = UILabel()
+        label.text = "단어장을 선택해 주세요 >"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .customBrown
+        return label
+    }()
+    
     private let wordTextFieldView = CustomTextFieldView(title: "단어", placeholder: "단어를 입력하세요")
+    
     private let meaningTextFieldView = CustomTextFieldView(title: "뜻", placeholder: "뜻을 입력하세요")
-    
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     // MARK: - Setup
-    
+
     private func setupView() {
         view.backgroundColor = UIColor(white: 0, alpha: 0.5)
         view.addSubview(modalView)
-        
+
         modalView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.height.equalToSuperview().multipliedBy(0.85)
             $0.bottom.equalToSuperview()
         }
-        
+
+        modalView.contentStackView.addArrangedSubview(selectVocaLabel)
         modalView.contentStackView.addArrangedSubview(wordTextFieldView)
         modalView.contentStackView.addArrangedSubview(meaningTextFieldView)
     }
-    
 }
