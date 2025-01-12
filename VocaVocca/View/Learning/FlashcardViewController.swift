6ce5fadee4ca54f0559379589f69f8d88d693ca5
name-mun <tains9153@gmail.com>
2025-01-12 02:10:23 -0800

    feat: FlashcardVC에 VM 연결

--- VocaVocca/View/Learning/FlashcardViewController.swift
+++ VocaVocca/View/Learning/FlashcardViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 class FlashcardViewController: UIViewController {
 
     // MARK: - UI 컴포넌트
     
     let flashcardView = FlashcardView()
+    let flashcardViewModel = FlashcardViewModel()
+    let disposeBag = DisposeBag()
 
     // MARK: - 생명주기 메서드
 
@@ -24,10 +27,24 @@ class FlashcardViewController: UIViewController {
         super.viewDidLoad()
         tabBarController?.tabBar.isHidden = true
         navigationController?.navigationBar.isHidden = true
+        
+        flashcardViewModel.subject
+            .filter { $0 == false }
+            .subscribe(onNext: { [weak self] _ in
+                self?.connectModal()
+
+            })
+            .disposed(by: disposeBag)
+        
+        flashcardViewModel.isCoachMarkDisabled()
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+    }
+
+    private func connectModal() {
         let coachmarkVC = CoachmarkViewController()
         coachmarkVC.modalPresentationStyle = .overFullScreen
         present(coachmarkVC, animated: false)
