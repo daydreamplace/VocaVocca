4d45adcddd053204a63d0d6983a545716157ae1e
name-mun <tains9153@gmail.com>
2025-01-12 02:29:14 -0800

    feat: 닫기 버튼 Rx 연결

--- VocaVocca/View/Learning/FlashcardViewController.swift
+++ VocaVocca/View/Learning/FlashcardViewController.swift
@@ -8,45 +8,59 @@
 import UIKit
 import SnapKit
 import RxSwift
+import RxCocoa
 
 class FlashcardViewController: UIViewController {
-
+    
     // MARK: - UI 컴포넌트
     
     let flashcardView = FlashcardView()
     let flashcardViewModel = FlashcardViewModel()
     let disposeBag = DisposeBag()
-
+    
     // MARK: - 생명주기 메서드
-
+    
     override func loadView() {
         view = flashcardView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBarController?.tabBar.isHidden = true
         navigationController?.navigationBar.isHidden = true
         
+        bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    // MARK: - bind
+    
+    private func bind() {
         flashcardViewModel.subject
             .filter { $0 == false }
             .subscribe(onNext: { [weak self] _ in
                 self?.connectModal()
-
+                
             })
             .disposed(by: disposeBag)
-        
         flashcardViewModel.isCoachMarkDisabled()
-
+        
+        flashcardView.closeButton.rx.tap
+            .bind { [weak self] in
+                self?.closeButtonTapped()
+            }
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-    }
-
+    
     private func connectModal() {
         let coachmarkVC = CoachmarkViewController()
         coachmarkVC.modalPresentationStyle = .overFullScreen
         present(coachmarkVC, animated: false)
+    }
+    
+    private func closeButtonTapped() {
+        navigationController?.popViewController(animated: true)
     }
 }
