2de4d3ab30e25f336dcab72b4bbb5cde33b5e0cb
name-mun <tains9153@gmail.com>
2025-01-13 20:51:22 +0900

    feat: 학습 결과 서브젝트 생성

--- VocaVocca/ViewModel/Learning/FlashcardViewModel.swift
+++ VocaVocca/ViewModel/Learning/FlashcardViewModel.swift
@@ -8,13 +8,19 @@
 import Foundation
 import RxSwift
 
+
+struct LearningResult {
+    let correctCount: Int
+    let IncorrectCount: Int
+}
+
 class FlashcardViewModel {
     
     let isCoachMarkDisabled = BehaviorSubject(value: false) // 사용자가 코치마크를 비활성화했는지 여부
     let currentVoca: BehaviorSubject<VocaData?> // 현재 단어
     let currentIndex = BehaviorSubject(value: 0) // 현재 카드의 인덱스
     let totalVocaCount: BehaviorSubject<Int> // 전체 단어 수
-    let isLastVoca: BehaviorSubject<Bool> // 마지막 단어인지 여부
+    let learningResult = PublishSubject<LearningResult>()
     
     private let allVocaData: [VocaData] // 전체 단어 데이터
     private var currentIndexValue = 0 // 인덱스 변경을 위한 변수
@@ -25,10 +31,10 @@ class FlashcardViewModel {
         allVocaData = data.words?.allObjects as? [VocaData] ?? []
         totalVocaCount = BehaviorSubject(value: allVocaData.count)
         currentVoca = BehaviorSubject(value: allVocaData.first)
-        isLastVoca = BehaviorSubject(value: currentIndexValue == allVocaData.count ? true : false)
         checkIfCoachMarkShouldBeDisabled()
     }
     
+    // 코치마크가 비활성화되었는지 확인
     private func checkIfCoachMarkShouldBeDisabled() {
         if UserDefaultsManager().isCoachMarkDisabled() {
             isCoachMarkDisabled.onNext(true)
@@ -37,29 +43,34 @@ class FlashcardViewModel {
         }
     }
     
+    // 맞은 단어 표시
     func markWordAsCorrect() {
+        correctWords.append(allVocaData[currentIndexValue])
         moveToNextVoca()
-        correctWords.append(allVocaData[currentIndexValue - 1])
     }
     
+    // 틀린 단어 표시
     func markWordsAsIncorrect() {
+        incorrectWords.append(allVocaData[currentIndexValue])
         moveToNextVoca()
-        incorrectWords.append(allVocaData[currentIndexValue - 1])
     }
     
+    // 다음 단어로 이동
     private func moveToNextVoca() {
-        currentIndexValue += 1
-        currentIndex.onNext(currentIndexValue)
-        currentVoca.onNext(allVocaData[currentIndexValue])
-        isLastVoca.onNext(currentIndexValue + 1 == allVocaData.count)
+        // 마지막 단어인지 확인
+        if currentIndexValue + 1 == allVocaData.count {
+            saveResults() // 결과 저장
+            let reslut = LearningResult(correctCount: correctWords.count, IncorrectCount: incorrectWords.count)
+            learningResult.onNext(reslut)
+        } else {
+            currentIndexValue += 1
+            currentIndex.onNext(currentIndexValue)
+            currentVoca.onNext(allVocaData[currentIndexValue])
+        }
     }
     
-    // 정답 결과 저장
+    // 학습 결과 저장
     func saveResults() {
-        print("correctWords")
-        print(correctWords)
-        print("incorrectWords")
-        print(incorrectWords)
         correctWords.forEach {
             CoreDataManager.shared.createRecordData(voca: $0, isCorrected: true, date: Date())
         }
