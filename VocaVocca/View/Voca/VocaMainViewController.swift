5066a360a90e08aaf3c72b07899ed0db46cdfb71
Eden <you8inpark@gmail.com>
2025-01-13 14:34:07 +0900

    refactor: addArrangedSubviews extension 사용

--- VocaVocca/View/Voca/VocaMainViewController.swift
+++ VocaVocca/View/Voca/VocaMainViewController.swift
@@ -47,4 +47,3 @@ extension VocaMainViewController: UITableViewDataSource {
     }
 }
 
-
--- VocaVocca/View/Voca/VocaModalViewController.swift
+++ VocaVocca/View/Voca/VocaModalViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 import SnapKit
 
 class VocaModalViewController: UIViewController {
-
+    
     // MARK: - Properties
-
+    
     private let modalView = CustomModalView(title: "새로운 단어 만들기", buttonTitle: "추가하기")
     
     private let selectVocaLabel: UILabel = {
@@ -25,28 +25,26 @@ class VocaModalViewController: UIViewController {
     private let wordTextFieldView = CustomTextFieldView(title: "단어", placeholder: "단어를 입력하세요")
     
     private let meaningTextFieldView = CustomTextFieldView(title: "뜻", placeholder: "뜻을 입력하세요")
-
+    
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-
+    
     // MARK: - Setup
-
+    
     private func setupView() {
         view.backgroundColor = UIColor(white: 0, alpha: 0.5)
         view.addSubview(modalView)
-
+        
         modalView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.height.equalToSuperview().multipliedBy(0.85)
             $0.bottom.equalToSuperview()
         }
-
-        modalView.contentStackView.addArrangedSubview(selectVocaLabel)
-        modalView.contentStackView.addArrangedSubview(wordTextFieldView)
-        modalView.contentStackView.addArrangedSubview(meaningTextFieldView)
+        
+        modalView.contentStackView.addArrangedSubviews(selectVocaLabel, wordTextFieldView, meaningTextFieldView)
     }
 }
