392e38b207a61c8054c04f67768af33fc01570a5
Eden <you8inpark@gmail.com>
2025-01-13 17:31:26 +0900

    feat: VocaModalViewModel 구현

--- VocaVocca/ViewModel/Voca/VocaModalViewModel.swift
+++ VocaVocca/ViewModel/Voca/VocaModalViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class VocaModalViewModel {
+    
+    // MARK: - Input
+    
+    let selectedVocaBook = BehaviorRelay<String?>(value: nil)
+    let word = BehaviorRelay<String>(value: "")
+    let meaning = BehaviorRelay<String>(value: "")
+    
+    
+    // MARK: - Output
+    
+    let isSaveEnabled: Observable<Bool>
+    let title: Observable<String>
+    let buttonTitle: Observable<String>
+    
+    private let coreDataManager = CoreDataManager.shared
+    private let disposeBag = DisposeBag()
+    
+    // Initialization
+    init() {
+        // 저장 버튼 활성화 여부 결정
+        isSaveEnabled = Observable
+            .combineLatest(word.asObservable(), meaning.asObservable(), selectedVocaBook.asObservable())
+            .map { !$0.isEmpty && !$1.isEmpty && $2 != nil }
+        
+        // 화면 타이틀과 버튼 텍스트 설정
+        title = Observable.just("새로운 단어 만들기")
+        buttonTitle = Observable.just("추가하기")
+    }
+    
+    // TODO: - NetworkManager 구현
+
+    
+    func handleSave() {
+        guard let vocaBook = selectedVocaBook.value, !word.value.isEmpty, !meaning.value.isEmpty else {
+            print("입력값이 부족합니다.")
+            return
+        }
+        
+        print("단어 추가: \(word.value), 뜻: \(meaning.value), 단어장: \(vocaBook)")
+        // TODO: - CoreData 저장 로직 추가
+    }
+}
